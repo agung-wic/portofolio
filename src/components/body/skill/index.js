0f22191bd01bc98c68099479021e7f75e724657b
agung-wic <wicaksonoagung05@gmail.com>
2021-09-24 11:20:55 +0700

    skill section done

--- src/components/body/skill/index.js
+++ src/components/body/skill/index.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import Separator from '../../common/separator'
+import { SkillsData } from '../../data/skill'
+import SkillCard from './skill-card'
+import './skill.css'
 
 function Skill() {
+    const data = SkillsData
     return (
-        <div>
-            This is Skills
+        <div className='skill'>
+            <Separator />
+            <label className='section-title'>Skills</label>
+            <div className='skill-container'>
+                {data.map((item)=>{
+                    return(
+                        <div className='skill-section'>
+                            <label className='skill-section-title'>{item.type}</label>
+                            <div className='skill-list'>
+                                {item.list.map((skill)=>{
+                                    return(
+                                        <SkillCard skill={skill} />
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+
+            </div>
         </div>
     )
 }
---
+++ src/components/data/skill.js
@@ -0,0 +1,57 @@
+export const SkillsData = [
+    {
+        type: "Web Development",
+        list: [
+            {
+                name: "Laravel",
+                icon: <i class="devicon-laravel-plain colored"></i>,
+            },
+            {
+                name: "Bootstrap",
+                icon: <i class="devicon-bootstrap-plain colored"></i>,
+            },
+            {
+                name: "ReactJS",
+                icon:<i class="devicon-react-original colored"></i>,
+            },
+            {
+                name: "Javascript",
+                icon: <i class="devicon-javascript-plain colored"></i>,
+            },
+            {
+                name: "MySQL",
+                icon: <i class="devicon-mysql-plain-wordmark colored"></i>,
+            },
+        ]
+    },
+    {
+        type: "Machine Learning",
+        list: [
+            {
+                name: "Python",
+                icon: <i class="devicon-python-plain colored"></i>,
+            },
+            {
+                name: "Tensorflow",
+                icon: <i class="devicon-tensorflow-original colored"></i>,
+            },
+        ]
+    },
+    {
+        type: "Other",
+        list: [
+            {
+                name: "C",
+                icon: <i class="devicon-c-plain colored"></i>,
+            },
+            {
+                name: "C++",
+                icon: <i class="devicon-cplusplus-plain colored"></i>,
+            },
+            {
+                name: "Go",
+                icon: <i class="devicon-go-original-wordmark colored"></i>,
+            },
+        ]
+    }
+]
